abd9e17fe90cf85f491b68b8860d67b70c64dd91
Konrad Pabjan <konradpabjan@github.com>
2020-04-23 17:24:32 +0200

    Update testDownloadArtifact.ps1

--- scripts/testDownloadArtifact.ps1
+++ scripts/testDownloadArtifact.ps1
@@ -1,20 +1,22 @@
-$file1 = "artifact-1-directory/artifact-path/world.txt"
-if(!(Test-Path -path $file1))
-{
-    Write-Error "artifact-1-directory/artifact-path/world.txt file does not exist"
-}
-if(!((Get-Content $file1) -ceq 'hello'))
-{
-    cat $file1
-    Write-Error "File contents of world.txt are incorrect"
-}
-$file2 = "artifact-2-directory/artifact-path/gzip.txt"
-if(!(Test-Path -path $file2))
-{
-    Write-Error "artifact-2-directory/artifact-path/gzip.txt does not exist"
-}
-if(!((Get-Content $file2) -ceq 'Some large amount of text that has a compression ratio that is greater than 100%. If greater than 100%, gzip is used to upload the file'))
-{
-    cat $file2
-    Write-Error "File contents of gzip.txt are incorrect"
-}
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+    echo "File path not provided"
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    echo "Expected file contents not provided"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Expected file $1 does not exist"
+    exit 1
+fi
+
+fileContent=$(cat $1)
+if [ "$2" != "$fileContent" ];then
+    echo "File contents are not correct, expected $2, recieved $fileContent"
+    exit 1
+fi
