81f212e978cd1a7b6fd82b3cf824d69fcc6f0daa
Konrad Pabjan <konradpabjan@github.com>
2020-04-23 19:35:07 +0200

    Update test-artifact-file.sh

--- scripts/test-artifact-file.sh
+++ scripts/test-artifact-file.sh
@@ -1,22 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$1" == "" ]; then
+path=$1
+expectedContent=$2
+
+if [ "$path" == "" ]; then
     echo "File path not provided"
     exit 1
 fi
 
-if [ "$2" == "" ]; then
+if [ "$content" == "" ]; then
     echo "Expected file contents not provided"
     exit 1
 fi
 
-if [ ! -f "$1" ]; then
-    echo "Expected file $1 does not exist"
+if [ ! -f "$path" ]; then
+    echo "Expected file $path does not exist"
     exit 1
 fi
 
-fileContent=$(cat $1)
-if [ "$2" != "$fileContent" ]; then
-    echo "File contents are not correct, expected $2, recieved $fileContent"
+actualContent=$(cat $path)
+if [ "$content" != "$fileContent" ];then
+    echo "File contents are not correct, expected $expectedContent, recieved $actualContent"
     exit 1
 fi
